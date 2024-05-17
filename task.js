c7bcdb88fc73e7e5471237d37c316a900600e970
dufarepraful16 <prafulwdufare@gmail.com>
2024-05-17 15:55:35 +0530

    task file added

---
+++ task.js
@@ -0,0 +1,24 @@
+// var number = 6;
+// var factorial = 1;
+// for(let i=1; i<=number; i++){
+//     factorial= factorial*i
+//     console.log("Factorial of", i, "is", factorial);
+// };
+
+// for (let i = 1; i <= number; i++) {
+//     factorial *= i;
+
+// }
+// console.log(`The factorial of ${number} is: ${factorial}`)
+
+
+
+// var start = 5;
+// var end = 10;
+// for(let i=1; i<=end; i++){
+//     multiplication=start*i
+//     console.log(multiplication)
+// }
+
+
+let user = prompt("Enter your Number")
